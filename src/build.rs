b6686951d1ff94ab45f6f25900ac2b4e09881bce
uuhan <xuminhui189@gmail.com>
2019-03-03 05:36:40 +0800

    update

--- src/build.rs
+++ src/build.rs
@@ -8,19 +8,31 @@ fn main() {
     if let Ok(t) = env::var("V8_TARGET") {
         v8dir = t.clone();
         v8lib = format!("{}/out.gn/x64.release.sample/obj", v8dir);
-    } else {
-        v8dir = ".".to_owned();
-        v8lib = format!("{}/lib", v8dir);
-    }
-    cc::Build::new()
-        .cpp(true)
-        .flag("-std=c++11")
-        .flag("-Wno-return-type-c-linkage")
-        .include(format!("{}/include", v8dir))
-        .file("./deps/api.cc")
-        .warnings(false)
-        .compile("librustjs.a");
+        cc::Build::new()
+            .cpp(true)
+            .flag("-std=c++11")
+            .flag("-Wno-return-type-c-linkage")
+            .include(format!("{}/include", v8dir))
+            .file("./deps/api.cc")
+            .warnings(false)
+            .compile("librustjs.a");
 
-    println!("cargo:rustc-link-search={}", v8lib);
-    println!("cargo:rustc-link-lib=static=v8_monolith");
+        println!("cargo:rustc-link-search={}", v8lib);
+        println!("cargo:rustc-link-lib=static=v8_monolith");
+    } else {
+        v8dir = "/usr/local".to_owned();
+        v8lib = format!("{}/lib", v8dir);
+        cc::Build::new()
+            .cpp(true)
+            .flag("-std=c++11")
+            .flag("-Wno-return-type-c-linkage")
+            .include(format!("{}/include", v8dir))
+            .file("./deps/api.cc")
+            .warnings(false)
+            .compile("librustjs.a");
+
+        println!("cargo:rustc-link-search={}", v8lib);
+        println!("cargo:rustc-link-lib=static=v8");
+        println!("cargo:rustc-link-lib=static=v8_libplatform");
+    }
 }
