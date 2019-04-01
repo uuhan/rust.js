8121d4f1bebbcdaa8f6e222d47a606264ae28976
uuhan <xuminhui189@gmail.com>
2019-04-01 21:06:40 +0800

    update

--- src/bin/rustjs.rs
+++ src/bin/rustjs.rs
@@ -1,17 +1,5 @@
-#[macro_use]
-extern crate log;
-extern crate env_logger;
 extern crate rustjs;
 
-use env_logger::Env;
-
 fn main() {
-    let env = Env::default()
-        .filter_or("LOG_LEVEL", "info")
-        .write_style("LOG_STYLE");
-    env_logger::Builder::from_env(env)
-        .default_format_module_path(true)
-        .init();
-
     rustjs::new_instance();
 }
